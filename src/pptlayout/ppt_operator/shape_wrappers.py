dfc07f0978f296e7ff6daff213e949440e02a12e
Tianyu Hu <tyrionhuu@gmail.com>
2024-10-25 18:00:47 +0800

    table wrapper

--- src/pptlayout/ppt_operator/shape_wrappers.py
+++ src/pptlayout/ppt_operator/shape_wrappers.py
@@ -1,3 +1,21 @@
+"""
+from python-pptx documentation:
+
+for real-life shapes, there are these nine types:
+
+shape shapes – auto shapes with fill and an outline
+text boxes – auto shapes with no fill and no outline
+placeholders – auto shapes that can appear on a slide layout or master and be inherited on slides that use that layout, allowing content to be added that takes on the formatting of the placeholder
+line/connector – as described above
+picture – as described above
+table – that row and column thing
+chart – pie chart, line chart, etc.
+smart art – not supported yet, although preserved if present
+media clip – video or audio
+
+"""
+
+
 class BaseShapeWrapper:
     def __init__(self, shape):
         self.shape_type = shape.shape_type
@@ -9,8 +27,8 @@ class BaseShapeWrapper:
         self.shape_id = shape.shape_id
 
     @property
-    def text_info(self) -> None:
-        pass
+    def text_info(self) -> str:
+        return ""
 
     @property
     def position_info(self) -> str:
@@ -21,9 +39,8 @@ class BaseShapeWrapper:
         return f"Size: width={self.width}, height={self.height}\n"
 
     @property
-    def style_info(self) -> None:
-        # return f"Name: {self.name}, Shape ID: {self.shape_id}\n"
-        pass
+    def style_info(self) -> str:
+        return f"Name: {self.name}, Shape ID: {self.shape_id}\n"
 
     @property
     def shape_description(self) -> str:
@@ -34,12 +51,53 @@ class BaseShapeWrapper:
             return f"{str(self.shape_type).split(' ')[0].strip()}\n"
 
     def __repr__(self):
-        description = ""
-        description += self.shape_description
-        description += self.size_info
+        content = ""
+        content += self.shape_description
+        content += self.size_info
         if self.text_info is not None:
-            description += self.text_info
-        description += self.position_info
+            content += self.text_info
+        content += self.position_info
         if self.style_info is not None:
-            description += self.style_info
-        return description
+            content += self.style_info
+        return content
+
+
+class PictureWrapper(BaseShapeWrapper):
+    def __init__(self, shape, id=None):
+        super().__init__(shape)
+        self.image = shape.image
+        self.rotation = int(shape.rotation)
+        self.id = id
+
+    @property
+    def shape_description(self) -> str:
+        if self.id is not None:
+            return f"[Picture {self.id}]\n"
+        else:
+            return "[Picture]\n"
+
+    @property
+    def rotation_info(self) -> str:
+        return f"Rotation: {self.rotation}\n"
+
+
+class TableWrapper(BaseShapeWrapper):
+    def __init__(self, shape):
+        super().__init__(shape)
+        self.table = shape.table
+        self.rows = shape.table.rows
+        self.columns = shape.table.columns
+
+    @property
+    def text_info(self) -> str:
+        content = "Data:\n"
+        for row in self.rows:
+            content += "|"
+            for cell in row.cells:
+                content += f"{cell.text}|"
+            content += "\n"
+        return content
+
+    @property
+    def shape_description(self) -> str:
+        return f"[Table] with {len(self.rows)} rows and {len(self.columns)} columns\n"

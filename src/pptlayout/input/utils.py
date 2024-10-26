e2ab622ca0fca3361d4d3c7d39b127c1f906c643
胡天羽 <tyrionhuu@gmail.com>
2024-10-26 12:30:28 +0000

    utils

--- src/pptlayout/input/utils.py
+++ src/pptlayout/input/utils.py
@@ -4,7 +4,6 @@ import re
 
 import torch
 from pptx.enum.dml import MSO_FILL
-from pptx.enum.shapes import MSO_SHAPE_TYPE
 
 
 def get_fill_color(shape) -> str:
@@ -17,39 +16,6 @@ def get_fill_color(shape) -> str:
 
 def get_slide_size(presentation) -> tuple:
     return presentation.slide_width, presentation.slide_height
-
-
-def map_shape_type_to_label(shape_type) -> int:
-    # Define the mapping from MSO shape types to integer labels
-    mapping = {
-        MSO_SHAPE_TYPE.AUTO_SHAPE: 1,
-        MSO_SHAPE_TYPE.CALLOUT: 2,
-        MSO_SHAPE_TYPE.CANVAS: 3,
-        MSO_SHAPE_TYPE.CHART: 4,
-        MSO_SHAPE_TYPE.COMMENT: 5,
-        MSO_SHAPE_TYPE.DIAGRAM: 6,
-        MSO_SHAPE_TYPE.EMBEDDED_OLE_OBJECT: 7,
-        MSO_SHAPE_TYPE.FORM_CONTROL: 8,
-        MSO_SHAPE_TYPE.FREEFORM: 9,
-        MSO_SHAPE_TYPE.GROUP: 10,
-        MSO_SHAPE_TYPE.IGX_GRAPHIC: 11,
-        MSO_SHAPE_TYPE.INK: 12,
-        MSO_SHAPE_TYPE.INK_COMMENT: 13,
-        MSO_SHAPE_TYPE.LINE: 14,
-        MSO_SHAPE_TYPE.LINKED_OLE_OBJECT: 15,
-        MSO_SHAPE_TYPE.LINKED_PICTURE: 16,
-        MSO_SHAPE_TYPE.MEDIA: 17,
-        MSO_SHAPE_TYPE.OLE_CONTROL_OBJECT: 18,
-        MSO_SHAPE_TYPE.PICTURE: 19,
-        MSO_SHAPE_TYPE.PLACEHOLDER: 20,
-        MSO_SHAPE_TYPE.SCRIPT_ANCHOR: 21,
-        MSO_SHAPE_TYPE.TABLE: 22,
-        MSO_SHAPE_TYPE.TEXT_BOX: 23,
-        MSO_SHAPE_TYPE.TEXT_EFFECT: 24,
-        MSO_SHAPE_TYPE.WEB_VIDEO: 25,
-        MSO_SHAPE_TYPE.MIXED: 26,  # If you have mixed types
-    }
-    return mapping.get(shape_type, 0)  # Default to 0 if shape type is not found
 
 
 ID2LABEL = {
@@ -94,15 +60,50 @@ ID2LABEL = {
         8: "icon",
         9: "input",
     },
+    "ppt": {
+        1: "auto shape",
+        2: "callout",
+        3: "canvas",
+        4: "chart",
+        5: "comment",
+        6: "diagram",
+        7: "embedded ole object",
+        8: "form control",
+        9: "freeform",
+        10: "group",
+        11: "igx graphic",
+        12: "ink",
+        13: "ink comment",
+        14: "line",
+        15: "linked ole object",
+        16: "linked picture",
+        17: "media",
+        18: "ole control object",
+        19: "picture",
+        20: "placeholder",
+        21: "script anchor",
+        22: "table",
+        23: "text box",
+        24: "text effect",
+        25: "web video",
+        26: "mixed",
+    },
 }
 
 
-CANVAS_SIZE = {
-    "rico": (90, 160),
-    "publaynet": (120, 160),
-    "posterlayout": (102, 150),
-    "webui": (120, 120),
-}
+def canvas_size(domain: str, pptx_path: str) -> tuple:
+    if domain == "rico":
+        return 90, 160
+    if domain == "publaynet":
+        return 120, 160
+    if domain == "posterlayout":
+        return 102, 150
+    if domain == "webui":
+        return 120, 120
+    if domain == "ppt":
+        return get_slide_size(pptx_path)
+    else:
+        raise ValueError(f"Invalid domain: {domain}")
 
 
 def raw_data_path(x):

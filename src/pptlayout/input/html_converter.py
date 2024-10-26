62cede3868a2f8f3a1be387e0def845c4c2aa3d4
胡天羽 <tyrionhuu@gmail.com>
2024-10-26 05:15:36 +0000

    add sized element serializer class

--- src/pptlayout/input/html_converter.py
+++ src/pptlayout/input/html_converter.py
@@ -150,3 +150,61 @@ class BasicElementSerializer(Serializer):
 
     def build_input(self, data):
         return self.constraint_type[0] + super().build_input(data)
+
+
+class SizedElementSerializer(Serializer):
+    task_type = "generation conditioned on given element types and sizes"
+    constraint_type = ["Element Type and Size Constraint: "]
+    HTML_TEMPLATE_WITHOUT_UNKNOWN_TOKEN = (
+        '<div class="{}" style="width: {}px; height: {}px"></div>\n'
+    )
+    HTML_TEMPLATE_WITHOUT_UNKNOWN_TOKEN_WITH_INDEX = (
+        '<div class="{}" style="index: {}; width: {}px; height: {}px"></div>\n'
+    )
+
+    def _build_sequence_input(self, data):
+        labels = data["labels"]
+        bounding_boxes = data["discrete_gold_bounding_boxes"]
+        tokens = []
+
+        for index in range(len(labels)):
+            label = self.index2label[int(labels[index])]
+            bounding_box = bounding_boxes[index].tolist()
+            tokens.append(label)
+            if self.add_index_token:
+                tokens.append(str(index))
+            if self.add_unknown_token:
+                tokens += [self.unknown_token] * 2
+            tokens.extend(map(str, bounding_box[2:]))
+            if self.add_separation_token and index < len(labels) - 1:
+                tokens.append(self.separation_token)
+        return " ".join(tokens)
+
+    def _build_html_input(self, data):
+        labels = data["labels"]
+        bounding_boxes = data["discrete_gold_bounding_boxes"]
+        htmls = [HTML_PREFIX.format(self.canvas_width, self.canvas_height)]
+        if self.add_index_token and self.add_unknown_token:
+            _TEMPLATE = HTML_TEMPLATE_WITH_INDEX
+        elif self.add_index_token and not self.add_unknown_token:
+            _TEMPLATE = self.HTML_TEMPLATE_WITHOUT_UNKNOWN_TOKEN_WITH_INDEX
+        elif not self.add_index_token and self.add_unknown_token:
+            _TEMPLATE = HTML_TEMPLATE
+        else:
+            _TEMPLATE = self.HTML_TEMPLATE_WITHOUT_UNKNOWN_TOKEN
+
+        for index in range(len(labels)):
+            label = self.index2label[int(labels[index])]
+            bounding_box = bounding_boxes[index].tolist()
+            element = [label]
+            if self.add_index_token:
+                element.append(str(index))
+            if self.add_unknown_token:
+                element += [self.unknown_token] * 2
+            element.extend(map(str, bounding_box[2:]))
+            htmls.append(_TEMPLATE.format(*element))
+        htmls.append(HTML_SUFFIX)
+        return "".join(htmls)
+
+    def build_input(self, data):
+        return self.constraint_type[0] + super().build_input(data)

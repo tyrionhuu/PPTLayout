1dc67844a561f7353df1bd90d3000a529bbef5be
胡天羽 <tyrionhuu@gmail.com>
2024-10-26 05:09:21 +0000

    add basic element serializer class

--- src/pptlayout/input/html_converter.py
+++ src/pptlayout/input/html_converter.py
@@ -30,10 +30,10 @@ class Serializer:
         canvas_width: int,
         canvas_height: int,
         add_index_token: bool = True,
-        add_sep_token: bool = True,
-        sep_token: str = "|",
-        add_unk_token: bool = False,
-        unk_token: str = "<unk>",  # unknown token
+        add_separation_token: bool = True,
+        separation_token: str = "|",
+        add_unknown_token: bool = False,
+        unknown_token: str = "<unknown>",  # unknown token
     ):
         self.input_format = input_format
         self.output_format = output_format
@@ -41,20 +41,20 @@ class Serializer:
         self.canvas_width = canvas_width
         self.canvas_height = canvas_height
         self.add_index_token = add_index_token
-        self.add_sep_token = add_sep_token
-        self.sep_token = sep_token
-        self.add_unk_token = add_unk_token
-        self.unk_token = unk_token
+        self.add_separation_token = add_separation_token
+        self.separation_token = separation_token
+        self.add_unknown_token = add_unknown_token
+        self.unknown_token = unknown_token
 
     def build_input(self, data):
-        if self.input_format == "seq":
-            return self._build_seq_input(data)
+        if self.input_format == "sequence":
+            return self._build_sequence_input(data)
         elif self.input_format == "html":
             return self._build_html_input(data)
         else:
             raise ValueError(f"Unsupported input format: {self.input_format}")
 
-    def _build_seq_input(self, data):
+    def _build_sequence_input(self, data):
         raise NotImplementedError
 
     def _build_html_input(self, data):
@@ -63,12 +63,12 @@ class Serializer:
     def build_output(
         self, data, label_key="labels", bounding_box_key="discrete_gold_bounding_boxes"
     ):  # bounding_box_key: bounding box key
-        if self.output_format == "seq":
-            return self._build_seq_output(data, label_key, bounding_box_key)
+        if self.output_format == "sequence":
+            return self._build_sequence_output(data, label_key, bounding_box_key)
         elif self.output_format == "html":
             return self._build_html_output(data, label_key, bounding_box_key)
 
-    def _build_seq_output(self, data, label_key, bounding_box_key):
+    def _build_sequence_output(self, data, label_key, bounding_box_key):
         labels = data[label_key]
         bounding_boxes = data[bounding_box_key]
         tokens = []
@@ -80,8 +80,8 @@ class Serializer:
             if self.add_index_token:
                 tokens.append(str(index))
             tokens.extend(map(str, bounding_box))
-            if self.add_sep_token and index < len(labels) - 1:
-                tokens.append(self.sep_token)
+            if self.add_separation_token and index < len(labels) - 1:
+                tokens.append(self.separation_token)
         return " ".join(tokens)
 
     def _build_html_output(self, data, label_key, bounding_box_key):
@@ -100,3 +100,53 @@ class Serializer:
             htmls.append(_TEMPLATE.format(*element))
         htmls.append(HTML_SUFFIX)
         return "".join(htmls)
+
+
+class BasicElementSerializer(Serializer):
+    task_type = "generation conditioned on given element types"
+    constraint_type = ["Element Type Constraint: "]
+    HTML_TEMPLATE_WITHOUT_UNKNOWN_TOKEN = '<div class="{}"></div>\n'
+    HTML_TEMPLATE_WITHOUT_UNKNOWN_TOKEN_WITH_INDEX = (
+        '<div class="{}" style="index: {}"></div>\n'
+    )
+
+    def _build_sequence_input(self, data):
+        labels = data["labels"]
+        tokens = []
+
+        for idx in range(len(labels)):
+            label = self.index2label[int(labels[idx])]
+            tokens.append(label)
+            if self.add_index_token:
+                tokens.append(str(idx))
+            if self.add_unknown_token:
+                tokens += [self.unknown_token] * 4
+            if self.add_separation_token and idx < len(labels) - 1:
+                tokens.append(self.separation_token)
+        return " ".join(tokens)
+
+    def _build_html_input(self, data):
+        labels = data["labels"]
+        htmls = [HTML_PREFIX.format(self.canvas_width, self.canvas_height)]
+        if self.add_index_token and self.add_unknown_token:
+            _TEMPLATE = HTML_TEMPLATE_WITH_INDEX
+        elif self.add_index_token and not self.add_unknown_token:
+            _TEMPLATE = self.HTML_TEMPLATE_WITHOUT_UNKNOWN_TOKEN_WITH_INDEX
+        elif not self.add_index_token and self.add_unknown_token:
+            _TEMPLATE = HTML_TEMPLATE
+        else:
+            _TEMPLATE = self.HTML_TEMPLATE_WITHOUT_UNKNOWN_TOKEN
+
+        for idx in range(len(labels)):
+            label = self.index2label[int(labels[idx])]
+            element = [label]
+            if self.add_index_token:
+                element.append(str(idx))
+            if self.add_unknown_token:
+                element += [self.unknown_token] * 4
+            htmls.append(_TEMPLATE.format(*element))
+        htmls.append(HTML_SUFFIX)
+        return "".join(htmls)
+
+    def build_input(self, data):
+        return self.constraint_type[0] + super().build_input(data)

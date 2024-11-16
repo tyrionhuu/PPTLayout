ea625bdb1dc02182008c3dcccaca6531967f7fb0
Tianyu Hu <tyrionhuu@gmail.com>
2024-11-16 09:52:52 +0800

    add qwen2 vl support in call_ollama

--- src/pptlayout/llm/call_ollama.py
+++ src/pptlayout/llm/call_ollama.py
@@ -2,6 +2,8 @@ import os
 
 import ollama
 from ollama import Options
+from qwen_vl_utils import process_vision_info
+from transformers import AutoProcessor, Qwen2VLForConditionalGeneration
 
 # from json import dumps
 
@@ -23,26 +25,55 @@ def generate_slide_layout(
 ) -> str:
     model_name = get_model_name(model_name, images)
 
-    options = Options(
-        temperature=temperature,
-        num_ctx=max_tokens,
-        # top_p=top_p,
-    )
-
     if images is None:
-        response = ollama.generate(
-            model=model_name,
+        response = generate_no_image(
+            model_name=model_name,
             prompt=prompt,
-            options=options,
-            format="json" if json else "",
-        )["response"]
-        return response
+            temperature=temperature,
+            max_tokens=max_tokens,
+            json=json,
+        )
     else:
         if images is None:
             raise ValueError("image is None")
         for image in images:
             if not os.path.exists(image):
                 raise ValueError(f"Image file not found: {image}")
+        response = generate_with_image(
+            model_name=model_name,
+            prompt=prompt,
+            temperature=temperature,
+            max_tokens=max_tokens,
+            images=images,
+            json=json,
+        )
+    return response
+
+
+def generate_with_image(
+    model_name: str = "llama3.2-vision:11b",
+    prompt: str = "",
+    temperature: float = 0.5,
+    max_tokens: int = 32000,
+    images: list[str] | None = None,
+    json: bool = False,
+):
+    model_name = get_model_name(model_name, images)
+
+    if model_name == "Qwen2-VL-7B-Instruct":
+        response = generate_qwen2_vl(
+            prompt=prompt,
+            temperature=temperature,
+            max_tokens=max_tokens,
+            json=json,
+            images=images,
+        )
+        return response
+    else:
+        options = Options(
+            temperature=temperature,
+            num_ctx=max_tokens,
+        )
         response = ollama.generate(
             model=model_name,
             prompt=prompt,
@@ -51,6 +82,123 @@ def generate_slide_layout(
             format="json" if json else "",
         )["response"]
         return response
+
+
+def generate_no_image(
+    model_name: str = "llama3.1:8b",
+    prompt: str = "",
+    temperature: float = 0.5,
+    max_tokens: int = 32000,
+    json: bool = False,
+):
+    if model_name == "Qwen2-VL-7B-Instruct":
+        response = generate_qwen2_vl(
+            prompt=prompt,
+            temperature=temperature,
+            max_tokens=max_tokens,
+            json=json,
+        )
+        return response
+    else:
+        options = Options(
+            temperature=temperature,
+            num_ctx=max_tokens,
+        )
+        response = ollama.generate(
+            model=model_name,
+            prompt=prompt,
+            options=options,
+            format="json" if json else "",
+        )["response"]
+        return response
+
+
+def generate_qwen2_vl(
+    prompt: str = "",
+    temperature: float = 0.5,
+    max_tokens: int = 32000,
+    json: bool = False,
+    images: list[str] | None = None,
+):
+    model_dir = os.path.abspath("/data/share_weight/Qwen2-VL-7B-Instruct")
+
+    # default: Load the model on the available device(s)
+    model = Qwen2VLForConditionalGeneration.from_pretrained(
+        model_dir, torch_dtype="auto", device_map="auto"
+    )
+
+    processor = AutoProcessor.from_pretrained(model_dir)
+    messages = generate_qwen2_vl_message(images, prompt)
+    text = processor.apply_chat_template(
+        messages, tokenize=False, add_generation_prompt=True
+    )
+    image_inputs, video_inputs = process_vision_info(messages)
+    inputs = processor(
+        text=[text],
+        images=image_inputs,
+        videos=video_inputs,
+        padding=True,
+        return_tensors="pt",
+    )
+    inputs = inputs.to("cuda")
+    # Inference: Generation of the output
+    generated_ids = model.generate(
+        **inputs,
+        max_new_tokens=max_tokens,
+        temperature=temperature,
+        response_format={"type": "json_object"} if json else {"type": "text"},
+    )
+    generated_ids_trimmed = [
+        out_ids[len(in_ids) :]
+        for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+    ]
+    output_text = processor.batch_decode(
+        generated_ids_trimmed,
+        skip_special_tokens=True,
+        clean_up_tokenization_spaces=False,
+    )
+    # print(output_text)
+    return output_text
+
+
+def generate_qwen2_vl_message(
+    images: list[str] | None = None,
+    prompt: str = "",
+):
+    if images is None:
+        messages = [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "What is the problem with the layout of this slide?",
+                    },
+                ],
+            }
+        ]
+    else:
+        content = []
+        for image in images:
+            content.append(
+                {
+                    "type": "image",
+                    "image": image,
+                }
+            )
+        content.append(
+            {
+                "type": "text",
+                "text": prompt,
+            }
+        )
+        messages = [
+            {
+                "role": "user",
+                "content": content,
+            }
+        ]
+    return messages
 
 
 def get_model_name(model_name: str | None, images: list[str] | None) -> str:

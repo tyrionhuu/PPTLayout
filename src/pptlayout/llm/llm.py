e8cb270e1da7d9bf6c9039f3fabfc7b40be34982
Tianyu Hu <tyrionhuu@gmail.com>
2024-11-18 10:34:43 +0800

    fix llm.py

--- src/pptlayout/llm/llm.py
+++ src/pptlayout/llm/llm.py
@@ -1,10 +1,12 @@
 import os
 
 import ollama
+from modelscope import snapshot_download
 from ollama import Options
 from qwen_vl_utils import process_vision_info
 from transformers import AutoProcessor, Qwen2VLForConditionalGeneration
 
+model_dir = snapshot_download("qwen/Qwen2-VL-7B-Instruct")
 # from json import dumps
 
 # from tqdm import tqdm
@@ -23,7 +25,7 @@ def generate_slide_layout(
     json: bool = False,
     # top_p: float = 0.9,
 ) -> str:
-    model_name = get_model_name(model_name, images)
+    model_name = get_model_name(model_name=model_name, images=images)
 
     if images is None:
         response = generate_no_image(
@@ -58,7 +60,7 @@ def generate_with_image(
     images: list[str] | None = None,
     json: bool = False,
 ) -> str:
-    model_name = get_model_name(model_name, images)
+    model_name = get_model_name(model_name=model_name, images=images)
 
     if model_name == "Qwen2-VL-7B-Instruct":
         response = generate_qwen2_vl(
@@ -121,7 +123,7 @@ def generate_qwen2_vl(
     images: list[str] | None = None,
 ) -> str:
     # model_dir = os.path.abspath("/data/tianyuhu/models/Qwen/Qwen2.5-Coder-7B-Instruct-GPTQ-Int4")
-    model_dir = "/data/share_weight/Qwen2-VL-7B-Instruct"
+    # model_dir = "/data/share_weight/Qwen2-VL-7B-Instruct"
 
     # default: Load the model on the available device(s)
     model = Qwen2VLForConditionalGeneration.from_pretrained(
@@ -129,14 +131,15 @@ def generate_qwen2_vl(
         torch_dtype="auto",
         device_map="auto",
     )
+
     min_pixels = 256 * 28 * 28
     max_pixels = 1280 * 28 * 28
     processor = AutoProcessor.from_pretrained(
         model_dir, min_pixels=min_pixels, max_pixels=max_pixels
     )
-    messages = generate_qwen2_vl_message(images, prompt)
+    messages = generate_qwen2_vl_message(images=images, prompt=prompt)
     text = processor.apply_chat_template(
-        str(messages), tokenize=False, add_generation_prompt=True
+        messages, tokenize=False, add_generation_prompt=True
     )
     image_inputs, video_inputs = process_vision_info(messages)
     inputs = processor(
@@ -170,7 +173,7 @@ def generate_qwen2_vl(
 def generate_qwen2_vl_message(
     images: list[str] | None = None,
     prompt: str = "",
-):
+) -> list[dict]:
     if images is None:
         messages = [
             {
@@ -217,6 +220,17 @@ def get_model_name(model_name: str | None, images: list[str] | None) -> str:
     return model_name
 
 
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+test_image = "/data/tianyuhu/PPTLayout/notebooks/test_input/image.jpg"
+prompt = "What can be improved in the layout of the slide and how? Give me some specific suggestions on how to improve the layout of the slide."
+
+suggestion = generate_slide_layout(
+    model_name="Qwen2-VL-7B-Instruct",
+    prompt=prompt,
+    images=[test_image],
+    # json=True,
+)
+print(suggestion)
 # def process_pptx(pptx_path, output_dir, model_name="llama3.1:8b", temperature=0.5):
 #     """
 #     Extracts slide information from a PPTX file, generates layout suggestions using a specified model,

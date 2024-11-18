ef98f3a80277b11c8a69ba6fb0d28a31931c1017
Tianyu Hu <tyrionhuu@gmail.com>
2024-11-18 19:43:17 +0800

    update

--- src/pptlayout/llm/llm.py
+++ src/pptlayout/llm/llm.py
@@ -1,13 +1,10 @@
 import os
 
 import ollama
-
-# from modelscope import snapshot_download
 from ollama import Options
 from qwen_vl_utils import process_vision_info
 from transformers import AutoProcessor, Qwen2VLForConditionalGeneration
 
-# model_dir = snapshot_download("qwen/Qwen2-VL-7B-Instruct")
 model_dir = "/data/share_weight/Qwen2-VL-7B-Instruct"
 
 
@@ -225,60 +222,3 @@ def get_model_name(model_name: str | None, images: list[str] | None) -> str:
 #     # json=True,
 # )
 # print(suggestion)
-# def process_pptx(pptx_path, output_dir, model_name="llama3.1:8b", temperature=0.5):
-#     """
-#     Extracts slide information from a PPTX file, generates layout suggestions using a specified model,
-#     and saves the output to text files.
-
-#     Parameters:
-#     - pptx_path (str): Path to the PPTX file.
-#     - output_dir (str): Directory to save the output suggestions.
-#     - model_name (str): Name of the model to generate slide layout suggestions.
-#     - temperature (float): Temperature setting for the model to control randomness.
-
-#     Returns:
-#     - suggestions_list (list): List of layout suggestions for each slide.
-#     """
-#     # Run extractors to get slide information
-#     info = run_extractors(pptx_path, "emu")
-#     print(dumps(info, indent=4))
-
-#     # Prepare for storing suggestions
-#     suggestions_list = []
-
-#     for slide_info in tqdm(info["slides"], desc="Processing slides"):
-#         slide_id = slide_info["slide_id"]
-#         slide_output_dir = os.path.join(output_dir, f"{slide_id}")
-#         suggestions_file_path = os.path.join(slide_output_dir, "suggestions.txt")
-
-#         # Check if suggestions already exist
-#         if os.path.exists(suggestions_file_path):
-#             with open(suggestions_file_path, "r") as f:
-#                 suggestions = f.read()
-#             suggestions_list.append(suggestions)
-#             continue
-
-#         # Build prompt and generate suggestions
-#         prompt = build_slide_layout_suggestion_prompts(
-#             json_input=slide_info,
-#             slide_width=info["slide_width"],
-#             slide_height=info["slide_height"]
-#         )
-#         suggestions = generate_slide_layout(
-#             model_name=model_name,
-#             prompt=prompt,
-#             temperature=temperature,
-#         )
-#         suggestions_list.append(suggestions)
-
-#         # Save suggestions
-#         os.makedirs(slide_output_dir, exist_ok=True)
-#         with open(suggestions_file_path, "w") as f:
-#             f.write(suggestions)
-
-#     return suggestions_list
-
-# # Example usage
-# pptx_path = "/data/tianyuhu/PPTLayout/data/pptx/ZK7FNUZ33GBBCG7CFVYS56TQCTD72CJR.pptx"
-# output_dir = os.path.join(os.curdir, "test_output")
-# suggestions = process_pptx(pptx_path, output_dir)
